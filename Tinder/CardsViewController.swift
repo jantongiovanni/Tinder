88bf1ffac46a7b3efcc49afd65d688c33211d0d5
Joe Antongiovanni <joe@Joes-Mac.local>
2018-03-27 22:06:03 -0700

    card moves

--- Tinder/CardsViewController.swift
+++ Tinder/CardsViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class CardsViewController: UIViewController {
 
+    @IBOutlet weak var card: UIImageView!
+    var cardCenter: CGPoint!
     var cardInitialCenter: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didMoveCard(_:)))
+        card.isUserInteractionEnabled = true
+        card.addGestureRecognizer(panGestureRecognizer)
         // Do any additional setup after loading the view.
     }
 
@@ -24,17 +28,24 @@ class CardsViewController: UIViewController {
     }
     
     @IBAction func didMoveCard(_ sender: UIPanGestureRecognizer) {
-        //let location = sender.location(in: view)
-        //let velocity = sender.velocity(in: view)
-        //let translation = sender.translation(in: view)
+        let location = sender.location(in: view)
+        let velocity = sender.velocity(in: view)
+        let translation = sender.translation(in: view)
         
         if sender.state == .began {
             print("Gesture began")
+            cardCenter = card.center
         } else if sender.state == .changed {
             print("Gesture is changing")
+            card.center = CGPoint(x: cardCenter.x + translation.x, y: cardInitialCenter.y)
+
+            //card.transform = CGAffineTransform()
+            
+           // card.transform = view.transform.rotated(by angle: CGFloat(45 * M_PI / 180))
+
         } else if sender.state == .ended {
             print("Gesture ended")
-        }
+            card.transform = cardInitialCenter        }
     }
 
     /*

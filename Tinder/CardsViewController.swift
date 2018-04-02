3d7cbbeb011c603eec08f0374dfe6c7a92f7df4d
Joe Antongiovanni <joe@Joes-Mac.local>
2018-04-01 20:33:31 -0700

    card rotates and resets on release

--- Tinder/CardsViewController.swift
+++ Tinder/CardsViewController.swift
@@ -13,6 +13,7 @@ class CardsViewController: UIViewController {
     @IBOutlet weak var card: UIImageView!
     var cardCenter: CGPoint!
     var cardInitialCenter: CGPoint!
+    var divisor: CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,9 @@ class CardsViewController: UIViewController {
         card.isUserInteractionEnabled = true
         card.addGestureRecognizer(panGestureRecognizer)
         // Do any additional setup after loading the view.
+        
+        divisor = (view.frame.width / 2)/0.61
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,9 +32,10 @@ class CardsViewController: UIViewController {
     }
     
     @IBAction func didMoveCard(_ sender: UIPanGestureRecognizer) {
-        let location = sender.location(in: view)
-        let velocity = sender.velocity(in: view)
         let translation = sender.translation(in: view)
+        let xFromCenter = card.center.x - view.center.x
+        
+        card.transform = CGAffineTransform(rotationAngle: xFromCenter/divisor)
         
         if sender.state == .began {
             print("Gesture began")
@@ -38,15 +43,11 @@ class CardsViewController: UIViewController {
         } else if sender.state == .changed {
             print("Gesture is changing")
             card.center = CGPoint(x: cardCenter.x + translation.x, y: cardCenter.y)
-
-            //card.transform = CGAffineTransform()
-            
-           //card.transform = view.transform.rotated(by: CGFloat(M_PI / 180))
             
         } else if sender.state == .ended {
             print("Gesture ended")
-            //card.transform = CGAffineTransform.identity
-            //card.center = cardInitialCenter
+            card.transform = CGAffineTransform.identity
+            card.center = view.center
         }
     }
 
